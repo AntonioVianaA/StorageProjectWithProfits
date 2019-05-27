8ab977c453f2742430ac669c7eb1c25ffd411240
Aluno <Aluno@DOSO121740>
2019-05-27 20:01:43 -0300

    Sem erros Receita

--- Storage/StorageProject/Models/Receita.cs
+++ Storage/StorageProject/Models/Receita.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿
+using StorageProject.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
-namespace StorageProject.Models
+[Table("Receita")]
+
+public class Receita
 {
-    [Table("Receita")]
-    class Receita
+    public Receita()
     {
-        public Receita()
-        {
-            CadastradoEm = DateTime.Now;
-        }
 
-        [Key]
-        public int ReceitaId { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public string ModoPreparo { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public string Nome { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public int Rendimento { get; set; }
-        public DateTime CadastradoEm { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public DateTime TempoPreparo { get; set; }
+        CadastradoEm = DateTime.Now;
     }
+
+    [Key]
+    public int ReceitaID { get; set; }
+    public List<Ingredientes_Receita> Ingredientes_Receita { get; set; }
+    [Required(ErrorMessage = "Campo Obrigatório!")]
+    public string ModoPreparo { get; set; }
+    [Required(ErrorMessage = "Campo Obrigatório!")]
+    public string Nome { get; set; }
+    [Required(ErrorMessage = "Campo Obrigatório!")]
+    public int Rendimento { get; set; }
+    public DateTime CadastradoEm { get; set; }
+    [Required(ErrorMessage = "Campo Obrigatório!")]
+    public DateTime TempoPreparo { get; set; }
 }
